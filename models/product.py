0a09816a4742a7ade5f185e07bb76ca81393555a
Grandis SYF <gra@dr.com>
2025-05-20 21:42:30 +0700

    refactor: rename product category model for clarity
    - Change model name to CompanyProductCategory
    
    - Update references in ProductTemplate
    
    - Introduce methods for managing company-specific categories

--- models/product.py
+++ models/product.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
-from odoo import models, fields, api, _
+from odoo import models, fields, api
 
-class ProductCategoryCompany(models.Model):
-    _name = 'product.category.company'
-    _description = 'Product Category per Company'
+class CompanyProductCategory(models.Model):
+    _name = 'company.product.category'
+    _description = 'Company-specific Product Category'
     
     product_id = fields.Many2one(
         'product.template', 
@@ -36,42 +36,49 @@ class ProductTemplate(models.Model):
     _inherit = 'product.template'
     
     company_category_ids = fields.One2many(
-        'product.category.company',
+        'company.product.category',
         'product_id',
         string='Categories per Company'
     )
     
-    # Override standard category field
-    original_categ_id = fields.Many2one(
-        related='categ_id', 
-        string='Original Category',
-        readonly=True
-    )
+    def _get_company_category(self):
+        self.ensure_one()
+        company_id = self.env.company.id
+        company_category = self.env['company.product.category'].search([
+            ('product_id', '=', self.id),
+            ('company_id', '=', company_id)
+        ], limit=1)
+        
+        if company_category:
+            return company_category.category_id
+        return self.categ_id
+    
+    def _set_company_category(self, category):
+        self.ensure_one()
+        company_id = self.env.company.id
+        company_category = self.env['company.product.category'].search([
+            ('product_id', '=', self.id),
+            ('company_id', '=', company_id)
+        ], limit=1)
+        
+        if company_category:
+            company_category.write({'category_id': category.id})
+        else:
+            self.env['company.product.category'].create({
+                'product_id': self.id,
+                'company_id': company_id,
+                'category_id': category.id
+            })
     
     @api.model
     def create(self, vals):
         res = super(ProductTemplate, self).create(vals)
         # Initialize company-specific category with the default category
         if 'categ_id' in vals and vals['categ_id']:
-            self.env['product.category.company'].create({
+            self.env['company.product.category'].create({
                 'product_id': res.id,
                 'company_id': self.env.company.id,
                 'category_id': vals['categ_id'],
             })
         return res
-    
-    def write(self, vals):
-        # If category is changing, update company-specific category
-        if 'categ_id' in vals:
-            for record in self:
-                company_id = self.env.company.id
-                company_category = self.env['product.category.company'].search([
-                    ('product_id', '=', record.id),
-                    ('company_id', '=', company_id)
-                ], limit=1)
-                
-                if company_category:
-                    company_category.write({'category_id': vals['categ_id']})
-                else:
-                    self.env['product
 
